a3c673f3651b0d5e375d14493204702ebff84064
Garima Gupta <gg3738200@gmail.com>
2025-09-11 23:11:08 +0530

    Create Q_2.c

---
+++ DAY_1/Q_2.c
@@ -0,0 +1,17 @@
+Q2: Write a program to input two numbers and display their sum, difference, product, and quotient.
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter two numbers: ");
+    scanf("%d%d",&a,&b);
+    printf("Sum=%d\n",a + b);
+    printf("Diff=%d\n", a - b);
+    printf("Product=%d\n", a* b);
+    if(b!= 0)
+    {
+    printf("Quotient=%d\n", a / b);}
+    else
+    {
+      printf("Quotient is undefined\n");
+    }
+}
