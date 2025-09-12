7cb9c2f31af17ebd612e6158ad8a0a43b9b35e66
Garima Gupta <gg3738200@gmail.com>
2025-09-12 22:14:21 +0530

    Create Q_1.c

---
+++ DAY_23/Q_1.c
@@ -0,0 +1,17 @@
+Q46: Write a program to print the following pattern:
+#include<stdio.h>
+int main()
+{
+    int n, i, j;
+    printf("Enter value of n: ");
+    scanf("%d", &n);
+    for(i=1; i <= n; i++)
+    {
+        for(j=1; j <= n; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+    }
