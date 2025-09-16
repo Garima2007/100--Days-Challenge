ff5dec66d75e7059fcd15700af37201fc2758d04
Garima Gupta <gg3738200@gmail.com>
2025-09-16 17:32:12 +0530

    Create Q_2.c

---
+++ DAY_27/Q_2.c
@@ -0,0 +1,45 @@
+Q54: Write a program to print the following pattern:
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with layers of stars as shown.
+
+*/
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    for (i = 1; i <= 1; i++)
+    {
+        printf("*");
+    }
+    printf("\n\n");
+    for (i = 1; i <= 3; i++)
+    {
+        printf("*");
+    }
+    printf("\n\n");
+    for (i = 1; i <= 5 ; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+
+    for (i = 1; i <= 7; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+    for (i = 1; i <= 3; i++)
+    {
+        printf("*");
+    }
+    printf("\n\n");
+    for (i = 1; i <= 1; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+    return 0;
+}
