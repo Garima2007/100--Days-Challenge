0001e2643dd3639fe047bbe09a3cd273962a881a
Garima Gupta <gg3738200@gmail.com>
2025-09-16 17:38:29 +0530

    Create Q_1.c

---
+++ DAY_27/Q_1.c
@@ -0,0 +1,53 @@
+Q53: Write a program to print the following pattern:
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    for (i = 1; i <= 1; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+    for (i = 1; i <= 3; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+    for (i = 1; i <= 5 ; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+
+    for (i = 1; i <= 7; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+    for (i = 1; i <= 9; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+    for (i = 1; i <= 7; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+    for (i = 1; i <= 5 ; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+    for (i = 1; i <= 3; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+    for (i = 1; i <= 1; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+    return 0;
+}
