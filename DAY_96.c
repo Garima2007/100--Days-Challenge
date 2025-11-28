b73d74357825a96cf59984a76b0c0a272c4c7f75
Garima Gupta <gg3738200@gmail.com>
2025-11-28 20:41:36 +0530

    Update DAY_96.c

--- DAY_96.c
+++ DAY_96.c
@@ -9,31 +9,48 @@ Name: Raj | ID: 11 | Joining Date: 12/05/2020
 
 */
 #include <stdio.h>
+#include <string.h>
 
-struct Date {
-    int day;
-    int month;
-    int year;
-};
+typedef enum { ELECTRONICS = 1, ACCESSORY, FOOD } Category;
+typedef enum { NOT_IDENTICAL, IDENTICAL } CompareResult;
 
-struct Employee {
-    char name[50];
+struct Item {
     int id;
-    struct Date joiningDate;
+    char name[20];
+    float price;
+    Category type;
 };
+
+CompareResult are_identical(struct Item item1, struct Item item2) {
+    if (item1.id != item2.id)
+        return NOT_IDENTICAL;
+
+    if (item1.price != item2.price)
+        return NOT_IDENTICAL;
+
+    if (item1.type != item2.type)
+        return NOT_IDENTICAL;
+
+    if (strcmp(item1.name, item2.name) != 0)
+        return NOT_IDENTICAL;
+
+    return IDENTICAL;
+}
 
 int main() {
-    struct Employee emp;
+    struct Item a = {101, "Laptop", 1200.50, ELECTRONICS};
+    struct Item b = {101, "Laptop", 1200.50, ELECTRONICS};
+    struct Item c = {102, "Mouse", 25.00, ACCESSORY};
 
-    printf("Enter employee details:\n");
-    printf("Name: ");
-    scanf("%s", emp.name);
-    printf("ID: ");
-    scanf("%d", &emp.id);
-    printf("Joining Date (day month year): ");
-    scanf("%d %d %d", &emp.joiningDate.day, &emp.joiningDate.month, &emp.joiningDate.year);
+    if (are_identical(a, b) == IDENTICAL)
+        printf("Struct a and Struct b are IDENTICAL.\n");
+    else
+        printf("Struct a and Struct b are NOT identical.\n");
 
-    printf("Name: %s | ID: %d | Joining Date: %02d/%02d/%04d\n", emp.name, emp.id, emp.joiningDate.day, emp.joiningDate.month, emp.joiningDate.year);
+    if (are_identical(a, c) == IDENTICAL)
+        printf("Struct a and Struct c are IDENTICAL.\n");
+    else
+        printf("Struct a and Struct c are NOT identical.\n");
 
     return 0;
 }
