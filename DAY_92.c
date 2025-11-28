ea19b9883c33f9ee92dd86f1b05e3799514ba2d5
Garima Gupta <gg3738200@gmail.com>
2025-11-28 20:49:05 +0530

    Update DAY_92.c

--- DAY_92.c
+++ DAY_92.c
@@ -10,6 +10,8 @@ Tabular list of all 5 students with their details
 */
 #include <stdio.h>
 
+typedef enum { NAME, ROLL, MARKS } Field;
+
 struct Student {
     char name[50];
     int roll_no;
@@ -21,16 +23,11 @@ int main() {
     int i;
 
     for (i = 0; i < 5; i++) {
-        printf("Enter details for student %d:\n", i + 1);
-        printf("Name: ");
-        scanf("%s", students[i].name);
-        printf("Roll: ");
+        scanf("%49s", students[i].name);
         scanf("%d", &students[i].roll_no);
-        printf("Marks: ");
         scanf("%d", &students[i].marks);
     }
 
-    printf("\nAll Student Details:\n");
     printf("Name\tRoll\tMarks\n");
     for (i = 0; i < 5; i++) {
         printf("%s\t%d\t%d\n", students[i].name, students[i].roll_no, students[i].marks);
